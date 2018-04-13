c81cf135d59f682a23eb8b58f6f1ddf059b17092
Daniel Andrlik <daniel@andrlik.org>
2018-04-12 23:21:45 -0400

    🚀 Add utility for syncing pipenv requirements to pip.
    Necessary because some environments like nanobox don't play well with pipenv based projects.

--- utility/sync_pipenv_to_req.sh
+++ utility/sync_pipenv_to_req.sh
@@ -8,13 +8,17 @@ where:
      -h/--help Show this help text
      -p FILENAME Specify what file should be used for production packages (default: requirements.txt)
      -d FILENAME Specify what file should be used for development packages (default: test_requirements.txt)
+     -l Lock/pin the versions (note, VCS packages will always be pinned.)
 "
+lock=false
 while [ ! $# -eq 0 ]
 do
+    shift_twice=true
     case "$1" in
         --help | -h)
             echo "$usage"
             exit
+            shift_twice=false
             ;;
         -p )
             PRODUCTION_FILE="$2"
@@ -22,14 +26,22 @@ do
         -d )
             DEV_FILE="$2"
             ;;
+        -l )
+            lock=true
+            shift_twice=false
+            ;;
         * )
             echo "You have specified an invalid option."
             echo "$usage"
             exit 1
             ;;
     esac
-    shift
-    shift
+    if [ "$shift_twice" = true ];then
+        shift
+        shift
+    else
+        shift
+    fi
 done
 
 
@@ -39,7 +51,12 @@ if [ "$PRODUCTION_FILE" != "requirements.txt" ]; then
     echo "Using production file of ${PRODUCTION_FILE}."
 fi
 echo "Syncing production requirement packages..."
-pipenv lock --requirements | grep -v "#" | cut -d' ' -f1 > $PRODUCTION_FILE
+if [ "$lock" = true ];then
+    echo "Pinning requirements..."
+    pipenv lock --requirements | grep -v "#" | cut -d' ' -f1 > $PRODUCTION_FILE
+else
+    pipenv lock --requirements | grep -v "#" | sed 's/==.*//' > $PRODUCTION_FILE
+fi
 echo "Syncing production VCS packages..."
 pipenv lock --requirements | grep "#" | sed 's/# //' >> $PRODUCTION_FILE
 echo "Finished production packages!"
@@ -49,7 +66,12 @@ if [ "$DEV_FILE" != "test_requirements.txt" ]; then
     echo "Using development file of ${DEV_FILE}."
 fi
 echo "Syncing dev requirement packages..."
-pipenv lock --dev --requirements | grep -v "#" | cut -d' ' -f1 > $DEV_FILE
+if [ "$lock" = true ];then
+    echo "Pinning requirements..."
+    pipenv lock --dev --requirements | grep -v "#" | cut -d' ' -f1 > $DEV_FILE
+else
+    pipenv lock --dev --requirements | grep -v "#" | sed 's/==.*//' > $DEV_FILE
+fi
 echo "Syncing dev VCS packages..."
 pipenv lock --dev --requirements | grep "#" | sed 's/# //' >> $DEV_FILE
 echo "Finished dev packages!"
