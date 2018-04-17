4189f65955c8afd5445d991bebf38b9a2b0d72a8
Daniel Andrlik <daniel@andrlik.org>
2018-04-17 17:42:27 -0400

    🚀 Add more support for nanobox deployments

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
 # run.py
 from waitress import serve
-from maceoutliner import wsgi
+from config import wsgi
+import os
+
+if 'DATA_DB_USER' in os.environ.keys() and os.environ['DATA_DB_USER'] == 'gonano':
+    # We are in a nanobox instance.
+    serve_kwargs = {
+        'host': '0.0.0.0',
+        'port': 8000,
+    }
+    init_file_target = '/app/tmp/app-initialized'
+else:
+    # We are in some other place, use a unix socket.
+    serve_kwargs = {
+        'unix_socket': '/tmp/nginx.socket',
+    }
+    init_file_target = '/tmp/app-initialized'
 
 # touch app-initialized
-open('/tmp/app-initialized', 'w').close()
+open(init_file_target, 'w').close()
 # start waitress
-serve(wsgi.application, unix_socket='/tmp/nginx.socket')
+serve(wsgi.application, **serve_kwargs)
